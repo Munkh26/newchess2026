dc070ef1963f7f4d712ab136ec2adace047ed317
Munkh26 <munkhbatmunkhsoyombo@gmail.com>
2026-04-22 15:04:21 +0000

    a

--- src/main/java/com/example/Board.java
+++ src/main/java/com/example/Board.java
@@ -9,6 +9,7 @@ import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
@@ -109,6 +110,9 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         board[0][5].put(new Bishop(false, RESOURCES_BBISHOP_PNG));
         board[0][6].put(new Knight(false, RESOURCES_BKNIGHT_PNG));
         board[0][7].put(new Rook(false, RESOURCES_BROOK_PNG));
+        for (int i = 0; i < 8; i++) {
+            board[1][i].put(new Pawn(false, RESOURCES_BPAWN_PNG));
+        }
         board[7][0].put(new Rook(true, RESOURCES_WROOK_PNG));
         board[7][1].put(new Knight(true, RESOURCES_WKNIGHT_PNG));
         board[7][2].put(new Bishop(true, RESOURCES_WBISHOP_PNG));
@@ -117,6 +121,9 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         board[7][5].put(new Bishop(true, RESOURCES_WBISHOP_PNG));
         board[7][6].put(new Knight(true, RESOURCES_WKNIGHT_PNG));
         board[7][7].put(new Rook(true, RESOURCES_WROOK_PNG));
+        for (int i = 0; i < 8; i++) {
+            board[6][i].put(new Pawn(true, RESOURCES_WPAWN_PNG));
+        }
     }
 
     public Square[][] getSquareArray() {
@@ -175,6 +182,17 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     }
 
     private boolean isInCheck(Boolean color) {
+        ArrayList<Piece> opposite = new ArrayList<Piece>();
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board[row][col].getColor() != currPiece.getColor()) {
+                    opposite.add(board[row][col].getOccupyingPiece());
+                }
+            }
+        }
+
+
+
         return false;
     }
 
